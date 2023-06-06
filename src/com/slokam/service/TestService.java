04517734d3d91790aa256f96ad8e0d788fd8a672
phanikumar <phanikumarnspk@gmail.com>
2023-06-06 12:09:16 +0530

    git practice purpose

---
+++ src/com/slokam/service/TestService.java
@@ -0,0 +1,13 @@
+package com.slokam.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestService {
+	
+	public static void main(String[] args) {
+		List<String> L = new ArrayList<String>();
+		System.out.println("Arraylist Object creation");
+	}
+
+}
